6604eda7c32c0be2ce87d97efecbe5e08d82c125
oldst <oldst@DESKTOP-80UUF96>
2020-05-09 21:46:52 +0200

    Add alert dialog in drawing activity which triggers when back button is pressed

--- app/src/main/java/com/example/mso_laboratorium_zdalne/DrawingActivity.java
+++ app/src/main/java/com/example/mso_laboratorium_zdalne/DrawingActivity.java
@@ -1,6 +1,9 @@
 package com.example.mso_laboratorium_zdalne;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,10 +14,19 @@ public class DrawingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
-
     }
 
-    void back(View view){
-        finish();
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Zakonczyc?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Tak", (dialog, which) -> {
+            super.onBackPressed();
+        });
+        builder.setNegativeButton("Nie", (dialog, which) -> {
+
+        });
+        builder.create().show();
     }
 }
